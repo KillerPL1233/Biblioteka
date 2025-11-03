c567f681a4cab371e00c2c30f8cdb9f0124ba956
KillerPl1233 <maciejdmitruk2709@gmail.com>
2025-11-03 18:49:39 +0100

    working database

---
+++ Biblioteka/Data/LibraryDbContext.cs
@@ -0,0 +1,50 @@
+﻿using Biblioteka.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Biblioteka.Data
+{
+    public class LibraryDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public LibraryDbContext(DbContextOptions<LibraryDbContext> options) : base(options) { }
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<BookTag> BookTags { get; set; }
+        public DbSet<BookFile> BookFiles { get; set; }
+        public DbSet<Rental> Rentals { get; set; }
+        public DbSet<QueueEntry> QueueEntries { get; set; }
+        public DbSet<SearchHistory> SearchHistories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<BookTag>()
+                .HasKey(bt => new { bt.BookId, bt.TagId });
+
+            builder.Entity<Rental>()
+                .Property(r => r.FineAmount)
+                .HasColumnType("decimal(18,2)");
+
+
+            builder.Entity<Category>()
+                .HasOne(c => c.ParentCategory)
+                .WithMany(c => c.Children)
+                .HasForeignKey(c => c.ParentCategoryId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<Book>()
+                .HasIndex(b => b.Title);
+            builder.Entity<Book>()
+                .HasIndex(b => b.Author);
+            builder.Entity<Book>()
+                .HasIndex(b => b.ISBN);
+
+            builder.Entity<Rental>()
+                .Property(r => r.State)
+                .HasConversion<string>();
+        }
+    }
+}
---
+++ Biblioteka/Models/Entities.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Identity;
+
+namespace Biblioteka.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public ICollection<Rental> Rentals { get; set; }
+        public ICollection<SearchHistory> SavedSearches { get; set; }
+    }
+
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string ISBN { get; set; }
+        public string Description { get; set; }
+        public string TableOfContents { get; set; } // wyciąg ze spisu treści
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+        public int Stock { get; set; }
+
+        public ICollection<BookTag> BookTags { get; set; }
+        public ICollection<BookFile> Files { get; set; }
+        public ICollection<Rental> Rentals { get; set; }
+        public ICollection<QueueEntry> QueueEntries { get; set; }
+    }
+
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int? ParentCategoryId { get; set; }
+        public Category ParentCategory { get; set; }
+        public ICollection<Category> Children { get; set; }
+        public ICollection<Book> Books { get; set; }
+    }
+
+    public class Tag
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<BookTag> BookTags { get; set; }
+    }
+
+    public class BookTag
+    {
+        public int BookId { get; set; }
+        public Book Book { get; set; }
+        public int TagId { get; set; }
+        public Tag Tag { get; set; }
+    }
+
+    public class BookFile
+    {
+        public int Id { get; set; }
+        public int BookId { get; set; }
+        public Book Book { get; set; }
+        public string FileName { get; set; }
+        public string Url { get; set; }
+        public string Description { get; set; }
+    }
+
+    public enum RentalState
+    {
+        InStock,
+        WaitingPickup,
+        Rented
+    }
+
+    public class Rental
+    {
+        public int Id { get; set; }
+        public int BookId { get; set; }
+        public Book Book { get; set; }
+
+        public string ApplicationUserId { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
+
+        public DateTime RequestedAt { get; set; }
+        public DateTime? PickupBy { get; set; }
+        public DateTime? RentedAt { get; set; }
+        public DateTime? DueAt { get; set; }
+        public decimal? FineAmount { get; set; }
+        public RentalState State { get; set; }
+    }
+
+    public class QueueEntry
+    {
+        public int Id { get; set; }
+        public int BookId { get; set; }
+        public Book Book { get; set; }
+
+        public string ApplicationUserId { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
+
+        public DateTime EnqueuedAt { get; set; }
+    }
+
+    public class SearchHistory
+    {
+        public int Id { get; set; }
+        public string ApplicationUserId { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
+        public string Query { get; set; }
+        public DateTime SavedAt { get; set; }
+    }
+}
